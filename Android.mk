7802ce3024180887983e3d707603563e4a2dedd7
Seonghoi Lee <mighty1231@gmail.com>
2019-09-09 13:44:33 +0900

    Modify Android.mk to generate jar file without odex file

--- Android.mk
+++ Android.mk
@@ -3,12 +3,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_DEX_PREOPT := false
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
-LOCAL_MODULE := monkey
+LOCAL_MODULE := ape
 include $(BUILD_JAVA_LIBRARY)
-
-################################################################
-include $(CLEAR_VARS)
-ALL_PREBUILT += $(TARGET_OUT)/bin/monkey
-$(TARGET_OUT)/bin/monkey : $(LOCAL_PATH)/monkey | $(ACP)
-	$(transform-prebuilt-to-target)
